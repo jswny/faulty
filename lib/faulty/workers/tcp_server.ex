8d6b2dee7ade13b01d99238ed9a6ee59badc52d4
Joe Sweeney <joe@j1.io>
2020-11-10 02:22:38 -0500

    Better gen_tcp handling

--- lib/faulty/workers/tcp_server.ex
+++ lib/faulty/workers/tcp_server.ex
@@ -21,17 +21,14 @@ defmodule Faulty.TCPServer do
 
   @impl true
   def init([address, port]) do
-    {:ok, listen_socket} = :gen_tcp.listen(port, TCPUtils.tcp_opts(address))
-
     address_string = Utils.address_tuple_to_string(address)
-
     pid = inspect(self())
 
     Logger.info(
-      "#{@prefix} started on address #{address_string} and port #{port} with PID #{pid}"
+      "#{@prefix} started with address #{address_string} and port #{port} with PID #{pid}"
     )
 
-    {:ok, socket} = :gen_tcp.accept(listen_socket)
+    socket = listen(address, port)
     {:ok, %{address: address, port: port, socket: socket}}
   end
 
@@ -50,5 +47,30 @@ defmodule Faulty.TCPServer do
   def handle_info({:tcp_error, _socket, reason}, state) do
     Logger.info("#{@prefix} connection closed because: #{reason}")
     {:noreply, state}
+  end
+
+  defp listen({:ok, listen_socket}, port) do
+    Logger.info("#{@prefix} started listening on port #{port}.")
+    accept_connection(listen_socket, port)
+  end
+
+  defp listen({:error, reason}, port) do
+    Logger.error("#{@prefix} could not listen on port #{port}, reason: #{reason}")
+    exit(:normal)
+  end
+
+  defp listen(address, port) do
+    :gen_tcp.listen(port, TCPUtils.tcp_opts(address))
+    |> listen(port)
+  end
+
+  # Accepts an incoming connection.
+  defp accept_connection({:ok, socket}, port) do
+    Logger.info("#{@prefix} accepted connection on #{port}.")
+    socket
+  end
+
+  defp accept_connection(listen_socket, port) do
+    :gen_tcp.accept(listen_socket) |> accept_connection(port)
   end
 end
