464bc3dc930f510194fa3777e22a69212d546459
Joe Sweeney <joe@j1.io>
2020-11-14 16:49:46 -0500

    Cache PLT in CI

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Faulty.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -25,6 +26,15 @@ defmodule Faulty.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp dialyzer() do
+    plt_core_path = "_build/#{Mix.env()}"
+
+    [
+      plt_core_path: plt_core_path,
+      plt_file: {:no_warn, "#{plt_core_path}/dialyzer.plt"}
     ]
   end
 end
